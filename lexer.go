a1d891b777f479a730af30d8d4955e2922b7f518
Andrew Cranston <andrew.cranston@gmail.com>
2024-01-15 11:15:49 +1100

    Fix floating point parsing in the lexer

--- lexer.go
+++ lexer.go
@@ -241,10 +241,20 @@ func (l *Lexer) readIdentifier() (TokenType, string) {
 
 func (l *Lexer) readNumber() string {
 	var numBuilder strings.Builder
-	for isDigit(l.ch) {
+	hasDecimal := false
+
+	for isDigit(l.ch) || (l.ch == '.' && !hasDecimal) {
+		if l.ch == '.' {
+			hasDecimal = true
+		}
 		numBuilder.WriteRune(l.ch)
 		l.readChar()
 	}
+
+	if hasDecimal && numBuilder.Len() == 1 { // Handle single '.' as an error or different token
+		return "." // Or handle appropriately, e.g., return an error or a different token type
+	}
+
 	return numBuilder.String()
 }
 
